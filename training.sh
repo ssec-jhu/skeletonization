26f6702d8fc71268d1031dfefbaf7039a29be97c
Vladimir Kluzner <vkluzner@jhu.edu>
2024-12-12 20:56:47 +0000

    Add changes for training on SciServer

---
+++ training.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Change working directory
+cd /home/idies/workspace/Storage/vkluzner/persistent/skeletonization
+
+pip install torchvision albumentations easydict
+
+# Run the Python script
+#skelneton/bin/python main.py
+/home/idies/miniconda3/bin/python main.py
